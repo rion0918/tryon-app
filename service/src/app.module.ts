d7a8aac853a3f03429eb45242fc5fc6b5bd40345
りおん@象徴-メッシ <h.rion.0910@gmail.com>
2025-04-24 20:44:04 +0900

    不要なファイル削除

--- service/src/app.module.ts
+++ service/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
-import { HelloModule } from './hello/hello.module';
 import { AuthModule } from './auth/auth.module';
 import { TryOnModule } from './tryon/tryon.module';
 
